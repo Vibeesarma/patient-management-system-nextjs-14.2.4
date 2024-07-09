297b5e433c5ad3f60cef70cf2a7d20af72d50569
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2024-07-09 18:18:23 +0100

    feat🌱:  add pas key block

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModel from "@/components/PasskeyModel";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <main className="flex h-screen max-h-screen">
-      {/* TODO: add otp verification | passskey model */}
+      {isAdmin && <PasskeyModel />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
